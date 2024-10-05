eb93be7c96d383252d9e17fd9f566604497c5c7d
Yogesh Chaurasia <cy.cyberops@gmail.com>
2024-10-05 17:07:07 +0530

    A3

--- main.tf
+++ main.tf
@@ -21,6 +21,18 @@ provider "azurerm" {
   #use_oidc = true
 }
 
+resource "azurerm_storage_account" "yogesh_test_storage_account" {
+  name                     = "yogesh-test-storage-account"
+  resource_group_name      = "rg-terraform-github-actions-state"
+  location                 = "South India"
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+  tags = {
+    environment = "production"
+  }
+}
+
 # Define any Azure resources to be created here. A simple resource group is shown here as a minimal example.
 resource "azurerm_resource_group" "rg-aks" {
   name     = var.resource_group_name
